0d087ee21fcb3f82af82167d4fdfeb384ee3b4a8
arunaap <it20657796@my.sliit.lk>
2022-09-06 12:35:17 +0530

    Design Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,9 @@
+import Featured from "../../components/featured/Featured"
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
 import Header from "../../components/header/Header"
+import MailList from "../../components/mailList/MailList"
 import Navbar from "../../components/navbar/Navbar"
+import PropertyList from "../../components/propertyList/PropertyList"
 import "./home.css"
 
 const Home = () => {
@@ -7,6 +11,15 @@ const Home = () => {
         <div>
             <Navbar/>
             <Header/>
+
+            <div className="homeContainer">
+                <Featured/>
+                <h1 className="homeTitle">Brows By property type</h1>
+                <PropertyList/>
+                <h1 className="homeTitle">text</h1>
+                <FeaturedProperties/>
+                <MailList/>
+            </div>
         </div>
     )
 }
