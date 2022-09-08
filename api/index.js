90ad4e1ed2adb425b7f2cf7d2d6abec8d4c81469
it20649180 <88694471+it20649180@users.noreply.github.com>
2022-09-08 16:55:20 +0530

    My 1st commit

--- api/index.js
+++ api/index.js
@@ -27,12 +27,35 @@ app.get("/users", (req,res)=>{
     res.send("hello first request")
 })
 
+
+
 //middleware
+
+app.use(express.json())
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 app.use("/api/vehicles", vehiclesRoute);
+
+
+
+
+app.use((err,req,res,next)=>{
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "something went wrong";
+ return res.status(errorStatus).json({
+        Success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+
+});
+
+
+
 
 
 app.listen(8800, ()=>{
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,8 +1,27 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import Room from "../models/Room.js";
 
 const router = express.Router();
 
-//hgdjjshqjabsja
-//njhfjshkdshk
+//CREATE
+router.post("/", createRoom);
+
+
+//UPDATE
+router.put("/:id", updateRoom);
+
+
+//DELETE
+router.delete("/:id", deleteRoom);
+
+
+
+//GET
+router.get("/:id", getRoom);
+
+//GET ALL
+router.get("/", getRooms);
+
 
 export default router
