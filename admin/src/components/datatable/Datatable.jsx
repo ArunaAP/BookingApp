27399c6b2a0b9214ac179d555d2f6f93fbaf8e2f
NamalTharindu <101260728+NamalTharindu@users.noreply.github.com>
2022-09-16 20:23:47 +0530

    user add function and delete function

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -1,14 +1,32 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
+import useFetch from "../../hooks/useFetch"
+import { useEffect } from "react";
+import axios from "axios";
+
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
 
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
+  const location = useLocation();
+  const path = location.pathname.split("/")[1];
+  const [list , setList] = useState();
+   const {data , loading , error} = useFetch(`/${path}`)
+
+   useEffect(() => {
+          setList(data);
+   },[data])
+
+  const handleDelete = async (id) => {
+
+    try{
+          await axios.delete(`/${path}/${id}`);
+    }catch(err){  }
+
+     setList(list.filter((item) => item._id !== id));
+
   };
 
   const actionColumn = [
@@ -24,10 +42,16 @@ const Datatable = () => {
             </Link>
             <div
               className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
+              onClick={() => handleDelete(params.row._id)}
             >
               Delete
             </div>
+
+            <Link to="/users/new" style={{ textDecoration: "none" }}>
+              <div className="viewButton">Update</div>
+            </Link>
+
+
           </div>
         );
       },
@@ -43,11 +67,12 @@ const Datatable = () => {
       </div>
       <DataGrid
         className="datagrid"
-        rows={data}
+        rows={list}
         columns={userColumns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
         checkboxSelection
+        getRowId={row=>row._id}
       />
     </div>
   );
